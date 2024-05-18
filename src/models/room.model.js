08c65487c28012323135484a41880d916bfed40d
Mingtindu <mingmartu@gmail.com>
2024-05-18 20:46:09 +0545

    feat: Room model added

---
+++ src/models/room.model.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose'
+
+const roomSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+
+    },
+    location:{
+        type:String,
+        required:true,
+    },
+    capacity:{
+        type:Number,
+        required:true,
+    },
+    description:{
+        type:String,
+        required:true,
+    },
+    amenities:{
+        type:String,
+
+    },
+    image:{
+        type:String,//url of image 
+        required:true,
+    },
+    availability:{
+        type:Boolean,
+        required:true,
+    }
+})
