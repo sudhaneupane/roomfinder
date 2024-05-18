eda7f8c0155e5c26c127c8f27b3f6a9c4927f3f7
Mingtindu <mingmartu@gmail.com>
2024-05-18 21:44:51 +0545

    feat: room booking controller added

---
+++ src/controllers/booking.controler.js
@@ -0,0 +1,25 @@
+import { Booking } from "../models/booking.model";
+import { asyncHandler } from "../utils/asynchandler";
+
+
+const addBooking = asyncHandler(async(req,res)=>{
+    const {user_id,room_id,checkIn,noOfPeople,totalPrice,status}= req.body
+    if(!user_id||!room_id||!checkIn||!noOfPeople||!totalPrice||!status){
+        res.status(401).json({message:"All fields required"})
+    }
+    const createBooking = await Booking.create({
+        user_id,
+        room_id,
+        checkIn,
+        noOfPeople,
+        totalPrice,
+        status
+
+    })
+    if(!createBooking){
+        res.status(500).json({message:"Something went wrong while booking room"})
+    }
+    res.status(200).json({message:"Successfully booked room"})
+})
+
+export {addBooking}
