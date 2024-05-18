28b33cd278c7811da5e2d7cbc878197ba9926d41
Mingtindu <mingmartu@gmail.com>
2024-05-18 21:24:06 +0545

    feat: Booking model added'

---
+++ src/models/booking.model.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose'
+const bookingSchema = new mongoose.Schema({
+    user_id:{
+        type:mongoose.Types.Schema.user_id,
+        ref:'User',
+        required:true,
+    },
+    room_id:{
+        type:mongoose.Types.Schema.room_id,
+        ref:'Room',
+        required:true,
+
+    },
+    checkIn:{
+        type:Date,
+        required:true,
+    },
+    noOfPeople:{
+        type:Number,
+        required:true,
+    },
+    totalPrice:{
+        type:Number,
+        required:true,
+    },
+    status:{
+        type:String,
+        enum:['confirmed','pending','booked']
+    }
+
+})
+
+export const Booking = mongoose.model("Booking",bookingSchema)
