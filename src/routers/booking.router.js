c3a36e810ab18974a09e6e00c7a9fad281c8ff08
Mingtindu <mingmartu@gmail.com>
2024-05-19 11:53:57 +0545

    Update: get booking endpoint added

--- src/routers/booking.router.js
+++ src/routers/booking.router.js
@@ -1,6 +1,7 @@
-import { addBooking } from "../controllers/booking.controler";
+import { addBooking,getBooking } from "../controllers/booking.controler";
 import Router from 'express'
 const router = Router()
 router.route('/addBooking').post(addBooking)
+router.route('/getBooking').post(getBooking)
 
 export default router
